27ce8f2eb0544a4892d78a87b730f15e11213e8c
ListowiTwoThree <git@github.com:ListowiTwoThree/proxy.git>
2025-10-19 01:55:47 +0500

    Initial commit: Добавлены файлы игры

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const savedTheme = localStorage.getItem('theme') || 'dark';
+    document.documentElement.setAttribute('data-bs-theme', savedTheme);
+
+    // Логика для index.html
+    if (document.getElementById('startGame')) {
+        document.getElementById('themeLight').addEventListener('change', () => {
+            document.documentElement.setAttribute('data-bs-theme', 'light');
+            localStorage.setItem('theme', 'light');
+        });
+
+        document.getElementById('themeDark').addEventListener('change', () => {
+            document.documentElement.setAttribute('data-bs-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+        });
+
+        document.getElementById('startGame').addEventListener('click', () => {
+            const deck = document.querySelector('input[name="deckSize"]:checked').id.replace('deck', '');
+            const time = document.querySelector('input[name="time"]:checked').id.replace('time', '');
+            window.location.href = `game.html?deck=${deck}&time=${time}`;
+        });
+    }
+    
+    // Логика для game.html nahuy
+    if (document.getElementById('start-button')) {
+        const deckSize = urlParams.get('deck') || '32';
+        const time = urlParams.get('time') || '5';
+        document.getElementById('start-button').addEventListener('click', () => {
+            document.getElementById('start-button').disabled = true;
+            startGame(deckSize, time);
+        });
+    }
+});
